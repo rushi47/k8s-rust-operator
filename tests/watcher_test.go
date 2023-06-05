f565358b8bc63e130e7be161b0dd1bbd00a032db
rushi47 <rushikeshbutley@gmail.com>
2023-06-05 02:52:19 -0400

    Tests for service and docker file for testing

--- tests/watcher_test.go
+++ tests/watcher_test.go
@@ -43,13 +43,14 @@ func GetEndpintSlices() {
 }
 
 func TestGlobalService(t *testing.T) {
-	t.Logf("Testing if Service: %v is create", TEST_GLOBAL_SVC_NAME)
+	t.Logf("Testing if Service: %v is created", TEST_GLOBAL_SVC_NAME)
 
 	// Make request && make sure we get response from both cluster in X retries.
 	// Add retrieved responses inside map of string and make sure length is 2.
 	cluster := make(map[string]string)
 	tries := 0
 	for tries < TEST_RETRIES {
+		tries += 1
 		// Try service for couple of times
 		url := fmt.Sprintf("http://%v.%v", TEST_GLOBAL_SVC_NAME, TEST_NS)
 
@@ -74,4 +75,53 @@ func TestGlobalService(t *testing.T) {
 
 	//Make sure length of map is 2. This means we got response from both the clusters
 	assert.Equal(t, len(cluster), 2, " Looks like Global Service is unable to balance request beetween two clusters.")
+}
+
+func TestIndividualCluster(t *testing.T) {
+
+	t.Logf("Test specifc cluster, target1 & target2")
+
+	t.Run("Test Endpoint for Target1", func(t *testing.T) {
+		// echo-test-0-k3d-target1.echo-test-svc-global/echo
+		url := fmt.Sprintf("http://%v.%v", "echo-test-0-k3d-target1", TEST_GLOBAL_SVC_NAME)
+
+		resp, err := http.Get(url)
+
+		assert.NoError(t, err, "Unable to make HTTP request")
+		defer resp.Body.Close()
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+		body, err := ioutil.ReadAll(resp.Body)
+		assert.NoError(t, err, "Unable to read Response body")
+
+		respBody := string(body)
+		cluster_name := strings.Split(respBody, "Hello from: ")[1]
+
+		// Response will have cluster name
+		assert.Equal(t, true, strings.Contains(cluster_name, "target1"))
+
+	})
+
+	t.Run("Test Endpoint for Target2", func(t *testing.T) {
+		// echo-test-0-k3d-target1.echo-test-svc-global/echo
+		url := fmt.Sprintf("http://%v.%v", "echo-test-0-k3d-target2", TEST_GLOBAL_SVC_NAME)
+
+		resp, err := http.Get(url)
+
+		assert.NoError(t, err, "Unable to make HTTP request")
+		defer resp.Body.Close()
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+		body, err := ioutil.ReadAll(resp.Body)
+		assert.NoError(t, err, "Unable to read Response body")
+
+		respBody := string(body)
+		cluster_name := strings.Split(respBody, "Hello from: ")[1]
+
+		// Response will have cluster name
+		assert.Equal(t, true, strings.Contains(cluster_name, "target2"))
+
+	})
 }
